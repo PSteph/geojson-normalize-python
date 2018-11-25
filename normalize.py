d06d69fb5bc41947386616992fe6b4145d5ec192
vagrant <vagrant@vagrant.vm>
2018-11-25 02:21:34 +0000

    first commit

---
+++ normalize.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# normalize.py
+import io, ast, sys
+types = {
+    "Point": 'geometry',
+    "MultiPoint": 'geometry',
+    "LineString": 'geometry',
+    "MultiLineString": 'geometry',
+    "Polygon": 'geometry',
+    "MultiPolygon": 'geometry',
+    "GeometryCollection": 'geometry',
+    "Feature": 'feature',
+    "FeatureCollection": 'featurecollection'
+}
+
+
+def normalize(gj):
+    print (gj)
+    if (not gj or not gj["type"]):
+        return None
+
+    type = types[gj["type"]]
+
+    if not type:
+        return None
+    
+    if type == 'geometry':
+        return {
+            "type": 'FeatureCollection',
+            "features": [{
+                "type": 'Feature',
+                "properties": {},
+                "geometry": gj
+            }]
+        }
+    elif type == 'feature':
+        return {
+            "type": 'FeatureCollection',
+            "features": [gj]
+        }
+    elif type == 'featurecollection':
+        return gj
+
+# test
+if __name__ == "__main__":
+    
+    print (str(sys.argv[1]))
+    data = ""
+    for line in open(str(sys.argv[1])):
+        print (line)
+    print (normalize( open(ast.literal_eval(str(sys.argv[1]))) ))
+    # gj = {"type": "Point","coordinates": [125.6, 10.1]}
+    # normalize = Normalize(gj)
+    # print (normalize(gj))
